3e9e2f8f19ead015f7a4f7f5f35cc5f676ee2139
Amit-saha09 <amitamitsaha09@gmail.com>
2021-06-28 23:59:32 +0600

    doctor

---
+++ PharmaAssist/PharmaAssist/Controllers/DoctorController.cs
@@ -0,0 +1,33 @@
+﻿using PharmaAssist.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PharmaAssist.Controllers
+{
+    public class DoctorController : Controller
+    {
+        DoctorRepository contex = new DoctorRepository();
+        // GET: Doctor
+        public ActionResult Index()
+        {
+            return View(contex.GetAll());
+        }
+        public ActionResult Details(int Id)
+        {
+            var p = new BlogPostRepository();
+            ViewData["userblog"] = p.GetUserPosts(Id);
+            return View(contex.Get(Id));
+        }
+
+        public ActionResult Profile(int Id)
+        {
+
+            var p = contex.Getuserinfo(Id);
+            return View(p);
+        }
+
+    }
+}
